81c570357c2c17fe2fc52124d54cf1102c9c2158
JackMarx <seriousfools@gmail.com>
2020-01-15 12:07:12 -0600

    routes, actions, views

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,21 @@
 class Api::ProductsController < ApplicationController
+  def all_products
+    @products = Product.all
+    render "all_products.json.jb"
+  end
+
+  def first_product
+    @product = Product.first
+    render 'first_product.json.jb'
+  end
+
+  def second_product
+    @product = Product.second
+    render 'second_product.json.jb'
+  end
+
+  def third_product
+    @product = Product.third
+    render 'third_product.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/all_products" => 'products#all_products'
+    get '/first_product' => 'products#first_product'
+    get '/second_product' => 'products#second_product'
+    get '/third_product' => 'products#third_product'
+  end
 end
