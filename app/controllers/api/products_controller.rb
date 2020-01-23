0439abbd8c93c8827d7d4176be64341b4cc13b7e
JackMarx <seriousfools@gmail.com>
2020-01-23 12:20:02 -0600

    sort and order

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,26 @@
 class Api::ProductsController < ApplicationController
   def index
     @products = Product.all
+
+    search_term = params[:search]
+    discount_option = params[:discount]
+    sort_attribute = params[:sort]
+    sort_order = params[:sort_order]
+
+    if search_term
+      @products = @products.where("name iLIKE ?", "%#{ search_term }%")
+    end
+
+    if discount_option == "true"
+      @products = @products.where("price < ?", 50)
+    end
+
+    if sort_attribute == "price" && sort_order == "desc"
+      @products = @products.order(price: :desc)
+    elsif sort_attribute == "price"
+      @products = @products.order(price: :asc)
+    end
+
     render 'index.json.jb'
   end
 
